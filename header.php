9eeda4a8b74efa3730749089ad9eb820b8c748d6
nagatomo <nagatomo.23599@gmail.com>
2026-09-23 09:25:19 +0900

    GTMコード（tnagano28.com/codejump/用）を本番・非ログイン限定で header.php に追加

--- header.php
+++ header.php
@@ -3,10 +3,25 @@
   <head>
     <meta charset="<?php bloginfo('charset'); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <?php if ( $_SERVER['HTTP_HOST'] !== 'localhost' && ! is_user_logged_in() ) : ?>
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P664RVZP');</script>
+<!-- End Google Tag Manager -->
+    <?php endif; ?>
     <?php wp_head(); ?>
   </head>
-  <body>
-    <header>
+  <body <?php body_class(); ?>>
+    <?php if ($_SERVER['HTTP_HOST'] !== 'localhost' && ! is_user_logged_in() ) : ?>
+      <!-- Google Tag Manager (noscript) -->
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P664RVZP"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <!-- End Google Tag Manager (noscript) -->
+    <?php endif; ?>
+    <header class="header">
       <div class="container">
         <h1 class="logo">
           <a href="/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="ロゴ画像" /></a>
